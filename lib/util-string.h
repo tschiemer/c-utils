fa46599246a881e573e1c6590873cfcfe9b1bf92
philip <me@filou.se>
2017-11-06 14:51:05 +0100

    minor additions

--- lib/util-string.h
+++ lib/util-string.h
@@ -24,16 +24,25 @@ inline void u16_to_bigendian( uint8_t * dst, uint16_t u16)
 	dst[1] = u16 & 0xFF;
 }
 
+inline void bigendian_to_u16( uint16_t * dst, uint8_t * src )
+{
+	*dst = (((uint16_t)src[0]) << 8) | (uint16_t)src[1];
+}
+
+
+
 inline void s16_to_bigendian( uint8_t * dst, int16_t s16 )
 {
 	dst[0] = (s16 >> 8) & 0xFF;
 	dst[1] = s16 & 0xFF;
 }
 
-inline void bigendian_to_u16( uint16_t * dst, uint8_t * src )
+inline void bigendian_to_s16( int16_t * dst, uint8_t * src )
 {
-	*dst = (((uint16_t)src[0]) << 8) | (uint16_t)src[1];
+	*dst = (((int16_t)src[0]) << 8) | (int16_t)src[1];
 }
+
+
 
 inline void u24_to_bigendian( uint8_t * dst, uint32_t u24)
 {
@@ -47,6 +56,8 @@ inline void bigendian_to_u24( uint32_t * dst, uint8_t * src )
 	*dst = (((uint32_t)src[0]) << 16) | (((uint32_t)src[1]) << 8) | (uint32_t)src[2];
 }
 
+
+
 inline void s24_to_bigendian( uint8_t * dst, int32_t s24 )
 {
 	dst[0] = (s24 >> 16) & 0xFF;
@@ -56,7 +67,7 @@ inline void s24_to_bigendian( uint8_t * dst, int32_t s24 )
 
 inline void bigendian_to_s24( int32_t * dst, uint8_t * src )
 {
-	*dst = (((int32_t)src[0]) << 16) | (((int32_t)src[1]) << 8) | (uint32_t)src[2];
+	*dst = (((int32_t)src[0]) << 16) | (((int32_t)src[1]) << 8) | (int32_t)src[2];
 	//if topmost bit set -> negative
 	if ( (src[0] & 0x80) == 0x80)
 	{
@@ -64,7 +75,9 @@ inline void bigendian_to_s24( int32_t * dst, uint8_t * src )
 	}
 }
 
-inline void u32_to_bigendian( uint8_t * dst, uint32_t u32)
+
+
+inline void u32_to_bigendian( uint8_t * dst, uint32_t u32 )
 {
 	dst[0] = (u32 >> 24) & 0xFF;
 	dst[1] = (u32 >> 16) & 0xFF;
@@ -74,8 +87,25 @@ inline void u32_to_bigendian( uint8_t * dst, uint32_t u32)
 
 inline void bigendian_to_u32( uint32_t * dst, uint8_t * src )
 {
-	*dst = (((uint32_t)src[0]) << 24) | (((uint32_t)src[1]) << 16) | (((uint32_t)src[2]) << 8) | (uint16_t)src[3];
+	*dst = (((uint32_t)src[0]) << 24) | (((uint32_t)src[1]) << 16) | (((uint32_t)src[2]) << 8) | (uint32_t)src[3];
 }
+
+
+
+inline void s32_to_bigendian( uint8_t * dst, int32_t s32 )
+{
+	dst[0] = (s32 >> 24) & 0xFF;
+	dst[1] = (s32 >> 16) & 0xFF;
+	dst[2] = (s32 >> 8) & 0xFF;
+	dst[3] = s32 & 0xFF;
+}
+
+inline void bigendian_to_s32( int32_t * dst, uint8_t * src)
+{
+	*dst = (((int32_t)src[0]) << 24) | (((int32_t)src[1]) << 16) | (((int32_t)src[2]) << 8) | (int32_t)src[3];
+}
+
+
 
 int32_t str_to_s32( uint8_t * str );
 uint32_t str_to_u32( uint8_t * str );
